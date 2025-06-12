90b14ba0f8838700b4fb5f79a415151fbbe3a60c
Charles Martin Jones <CHUCKJONESMARTIN@GMAIL.COM>
2025-06-12 13:59:53 -0600

    Remove git conflict markers from app/api/__init__.py

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -2,7 +2,6 @@ from .solve_legacy import router as solve_legacy
 from .quantum import router as quantum
 from .scroll import router as scroll
 from .schema import router as schema
-=======
 from .solve_legacy import router as solve_legacy
 from .quantum import router as quantum
 from .scroll import router as scroll
