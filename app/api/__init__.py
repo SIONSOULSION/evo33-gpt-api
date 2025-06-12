96c4a55866515d9d353690e1271fdd1f9e671565
Charles Martin Jones <CHUCKJONESMARTIN@GMAIL.COM>
2025-06-12 14:15:11 -0600

    Update __init__.py

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -2,7 +2,3 @@ from .solve_legacy import router as solve_legacy
 from .quantum import router as quantum
 from .scroll import router as scroll
 from .schema import router as schema
-from .solve_legacy import router as solve_legacy
-from .quantum import router as quantum
-from .scroll import router as scroll
-from .schema import router as schema
